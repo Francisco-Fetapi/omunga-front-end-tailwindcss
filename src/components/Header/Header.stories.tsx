aea7a6a43e7500cd6996a18bccb210a3910d5334
Francisco Fetapi <franciscofetapi10@gmail.com>
2023-04-25 13:52:27 +0100

    feat: project structere defined

---
+++ src/components/Header/Header.stories.tsx
@@ -0,0 +1,23 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import React from "react";
+
+import Header from "./Header";
+
+const meta: Meta<typeof Header> = {
+  title: "Components/Header",
+  component: Header,
+  tags: ["autodocs"],
+  parameters: {
+    // More on how to position stories at: https://storybook.js.org/docs/react/configure/story-layout
+    layout: "fullscreen",
+  },
+};
+
+type Story = StoryObj<typeof Header>;
+
+export const Primary = () => <Header>Primary</Header>;
+export const Secondary = () => <Header>Secondary</Header>;
+export const Tertiary = () => <Header>Tertiary</Header>;
+
+export default meta;
