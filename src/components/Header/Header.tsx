e21a7a35785e6e9eb8493c9c0dae24c952176f5c
Francisco Fetapi <franciscofetapi10@gmail.com>
2023-04-25 14:31:57 +0100

    feat: setting up story book, assets and tailwindcss

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,3 +1,23 @@
+import OmungaPhoto from "../../assets/Omunga.png";
+import Image from "next/image";
+import "../../styles/globals.css";
+
 export default function Header({ children }: React.PropsWithChildren) {
-  return <header>{children}</header>;
+  return (
+    <header className="h-[100px] shadow-lg shadow-gray-300 flex items-center justify-between">
+      <div>
+        <Image src={OmungaPhoto} width={70} height={40} alt="Imagem Omunga" />
+      </div>
+      <div className="flex justify-between items-center">
+        <ul className="flex items-center">
+          <li className="m-2 text-black/90">Página Inicial</li>
+          <li className="m-2 text-black/90">Artigos</li>
+          <li className="m-2 text-black/90">Sobre</li>
+        </ul>
+        <div>
+          <button className="bg-purple text-white2">Entrar</button>
+        </div>
+      </div>
+    </header>
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,10 +6,6 @@ module.exports = {
     "./src/app/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    colors: {
-      purple: "#8571ff",
-      white2: "#f8f8fa",
-    },
     fontFamily: {
       sans: ["Roboto", "sans-serif"],
       monospace: ["Nico Moji", "monospace"],
@@ -22,6 +18,10 @@ module.exports = {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      colors: {
+        purple: "#8571ff",
+        white2: "#f8f8fa",
       },
     },
   },
