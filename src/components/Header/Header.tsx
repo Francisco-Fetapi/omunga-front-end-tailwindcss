78e561e6520ff8af77f76024e4eb1b62ce0780f0
Francisco Fetapi <franciscofetapi10@gmail.com>
2023-04-28 11:09:26 +0100

    feat: making header styles

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,18 +3,21 @@ import Image from "next/image";
 
 export default function Header({ children }: React.PropsWithChildren) {
   return (
-    <header className="h-[100px] shadow-lg shadow-gray-300 flex items-center justify-between">
+    <header className="h-[80px] shadow-lg shadow-gray-300 flex items-center pl-16">
       <div>
-        <Image src={OmungaPhoto} width={70} height={90} alt="Imagem Omunga" />
+        <Image src={OmungaPhoto} width={130} height={150} alt="Imagem Omunga" />
       </div>
-      <div className="flex justify-between items-center">
-        <ul className="flex items-center">
-          <li className="m-2">Página Inicial</li>
-          <li className="m-2">Artigos</li>
-          <li className="m-2">Sobre</li>
-        </ul>
-        <div>
-          <button className="bg-purple text-white2">Entrar</button>
+      <div className="grow" />
+      <div className="grow h-full">
+        <div className="h-full flex justify-end items-center">
+          <ul className="flex items-center">
+            <li className="m-2">Página Inicial</li>
+            <li className="m-2">Artigos</li>
+            <li className="m-2">Sobre</li>
+          </ul>
+          <div className="ml-5 w-[35%] min-w-[140px] bg-purple h-full flex items-center justify-center">
+            <button>Entrar</button>
+          </div>
         </div>
       </div>
     </header>
