0a46b6c6ca9f47f248f350080bf110c2d7483f66
Francisco Fetapi <franciscofetapi10@gmail.com>
2023-06-18 11:17:46 +0100

    feat: question card controls created

--- src/components/CommonQuestions/QuestionCard.tsx
+++ src/components/CommonQuestions/QuestionCard.tsx
@@ -2,6 +2,7 @@
 import { Question } from "@/entities/Question";
 import getShortText from "@/helpers/getShortText";
 import React from "react";
+import { FaComments, FaThumbsUp } from "react-icons/fa";
 
 interface QuestionCardProps {
   question: Question;
@@ -9,7 +10,7 @@ interface QuestionCardProps {
 
 export default function QuestionCard({ question }: QuestionCardProps) {
   return (
-    <div className="bg-white shadow-2xl rounded-xl h-[200px] flex items-start px-8 py-6 gap-2">
+    <div className="bg-white shadow-2xl rounded-xl  flex items-start px-8 py-6 gap-2">
       <img
         src={question.user.profilePhoto}
         alt="Foto do usuario."
@@ -20,6 +21,17 @@ export default function QuestionCard({ question }: QuestionCardProps) {
         <p className="text-gray-500 text-md">
           {getShortText(question.content, 70)}
         </p>
+
+        <div className="join gap-4 mt-4">
+          <div className="btn btn-ghost text-gray-600 btn-sm join-item flex gap-2 items-center">
+            <FaThumbsUp />
+            Gostos
+          </div>
+          <div className="btn btn-ghost text-gray-600 btn-sm join-item flex gap-2 items-center">
+            <FaComments />
+            Comentários
+          </div>
+        </div>
       </div>
     </div>
   );
--- src/mocks/QuestionsMock.ts
+++ src/mocks/QuestionsMock.ts
@@ -11,7 +11,31 @@ export const mockQuestions: Question[] = [
       Quibusdam delectus eius facere eaque, pariatur nobis nisi, facilis
       incidunt recusandae odio modi, quaerat aliquam iste molestias officiis`,
     createdAt: new Date(2021, 4, 27),
-    user: usersMock[1],
-    questioner: usersMock[0],
+    user: usersMock[0],
+    questioner: usersMock[1],
+  },
+  {
+    id: "2",
+    title: "Faz sentido apender Clean Architecture logo no começo?",
+    content: `Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cumque tempore
+      odio dolores eum iste beatae quos voluptatem quam molestias iure maxime
+      consequuntur eaque, nostrum non illum, facere adipisci, placeat et.
+      Quibusdam delectus eius facere eaque, pariatur nobis nisi, facilis
+      incidunt recusandae odio modi, quaerat aliquam iste molestias officiis`,
+    createdAt: new Date(2021, 4, 27),
+    user: usersMock[2],
+    questioner: usersMock[3],
+  },
+  {
+    id: "3",
+    title: "Qual devo escolher React.js/Vue.js/Angular.js ?",
+    content: `Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cumque tempore
+      odio dolores eum iste beatae quos voluptatem quam molestias iure maxime
+      consequuntur eaque, nostrum non illum, facere adipisci, placeat et.
+      Quibusdam delectus eius facere eaque, pariatur nobis nisi, facilis
+      incidunt recusandae odio modi, quaerat aliquam iste molestias officiis`,
+    createdAt: new Date(2021, 4, 27),
+    user: usersMock[3],
+    questioner: usersMock[2],
   },
 ];
