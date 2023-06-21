f345e48bf24022f7151c56201288331e90393bde
Francisco Fetapi <franciscofetapi10@gmail.com>
2023-06-21 21:10:10 +0100

    feat: added comments on comment section component

--- src/components/Comments/Comment.tsx
+++ src/components/Comments/Comment.tsx
@@ -2,6 +2,7 @@
 import { Comment } from "@/entities/Comment";
 import Link from "next/link";
 import React from "react";
+import { FaComments, FaThumbsUp } from "react-icons/fa";
 
 interface CommentProps {
   comment: Comment;
@@ -22,12 +23,27 @@ export default function Comment({ comment }: CommentProps) {
         <Link href={`/profile/${comment.user.id}`}>
           <span className="font-bold">{comment.user.name}</span>
         </Link>
-        <time className="pl-2 text-xs opacity-50">12:45</time>
       </div>
       <div className="chat-bubble bg-gray-300 text-black/90">
         {comment.content}
       </div>
-      <div className="chat-footer"></div>
+      <div className="chat-footer w-[88%]">
+        <div className="mt-2 flex justify-between">
+          <div>
+            <div className="join gap-4">
+              <div className="btn btn-ghost text-gray-600 btn-xs join-item flex gap-2 items-center">
+                <FaThumbsUp />
+                Gostar
+              </div>
+              <div className="btn btn-ghost text-gray-600 btn-xs join-item flex gap-2 items-center">
+                <FaComments />
+                Respostas
+              </div>
+            </div>
+          </div>
+          <time className="pl-2 text-xs opacity-50">Há 2 minutos</time>
+        </div>
+      </div>
     </div>
   );
 }
